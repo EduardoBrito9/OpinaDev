c76ec10eb8b33ccaa685ce9300b8b5883cb8b907
Eduardo Brito <eduardobritodev@gmail.com>
2024-05-08 06:54:27 -0300

    estilizacao do modal de edit de comentario

--- src/componentes/comments/ModalComment.tsx
+++ src/componentes/comments/ModalComment.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import ButtonProfileOptions from "../elements/ButtonProfileOptions";
 import { FiTrash } from "react-icons/fi";
 import { RxPencil1 } from "react-icons/rx";
-import { CiLink } from "react-icons/ci";
 import { supabase } from "../../lib/helper/supabaseClient";
 
 const ModalComment: React.FC<{
@@ -25,7 +24,8 @@ const ModalComment: React.FC<{
         commentsColumn: commentEdit,
       })
       .eq("id", commentId);
-      setCommentEdit('')
+    setCommentEdit("");
+    setEditing(false);
     getComments();
   };
 
@@ -40,26 +40,24 @@ const ModalComment: React.FC<{
           <ButtonProfileOptions
             onclickButton={() => {
               setEditing(!editing);
+              console.log("hello world");
             }}
           >
             {" "}
             Edit <RxPencil1 className=" w-5 h-5" />
           </ButtonProfileOptions>
         </div>
-        <ButtonProfileOptions
-          onclickButton={() => {
-            console.log("oi");
-          }}
-        >
-          Share <CiLink className=" w-5 h-5" />
-        </ButtonProfileOptions>
       </div>
       {editing && (
-        <form onSubmit={editComment}>
-          <label htmlFor="edit"></label>
+        <form
+          className="flex flex-col gap-5 bg-black absolute top-[40%] left-[35%] border border-modalColor p-7"
+          onSubmit={editComment}
+        >
+          <label htmlFor="edit">Edit</label>
+          <span>Edite seu comentario</span>
           <input
             type="text"
-            className=" text-black p-5"
+            className=" text-white px-3 py-2 min-w-[450px] bg-transparent border border-modalColor rounded-md outline-none"
             value={commentEdit}
             onChange={({ target }) => {
               setCommentEdit(target.value);
@@ -67,6 +65,14 @@ const ModalComment: React.FC<{
             name="edit"
             id="edit"
           />
+          <div className=" space-x-2 flex justify-end">
+            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition text-stone-800h-10 px-4 py-2 border border-modalColor">
+              Cancelar
+            </button>
+            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition bg-yellow-400 text-stone-800 hover:bg-yellow-500 h-10 px-4 py-2">
+              Continuar
+            </button>
+          </div>
         </form>
       )}
     </section>
